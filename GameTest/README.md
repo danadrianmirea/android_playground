b6fa8c5caa9adfd494a96c7a7b7058e187a17fc0
Dan Adrian Mirea <damirea84@gmail.com>
2025-02-24 22:33:55 +0200

    add gametest

---
+++ GameTest/README.md
@@ -0,0 +1,1 @@
+"android-game-cpp-test" 
---
+++ GameTest/init.bat
@@ -0,0 +1,7 @@
+echo "android-game-cpp-test" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:danadrianmirea/android-game-cpp-test.git
+git push -u origin main
